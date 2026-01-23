f6dce4a2c8c749df0169067aa308db2c18d851e0
Gabriel Serpa <gabrielmadrugaserpa08@gmail.com>
2026-01-23 15:37:27 +0000

    Add files via upload

---
+++ ex27.java
@@ -0,0 +1,11 @@
+public class ex27 {
+    public static void main(String[] args) {
+        String idade = "30";
+
+        int idade2 = Integer.parseInt(idade);
+        System.out.println("Valor int:" + idade2);
+
+        float idade3 = Float.parseFloat(idade);
+        System.out.println("Valor float:" + idade3);
+    }
+}
