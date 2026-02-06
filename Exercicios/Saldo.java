a30ca129452c0668f3e7c44225d6bff0ed1d3ca7
Gabriel Serpa <gabrielmadrugaserpa08@gmail.com>
2026-02-06 16:24:46 +0000

    Add files via upload

---
+++ Exercicios/Saldo.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+public class Saldo {
+
+    private double saldo;
+
+    //construtor
+    public Saldo(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public double
+}
